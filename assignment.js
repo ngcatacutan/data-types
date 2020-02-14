faf1880cfe25eed923a5a747083f10cc1d00477b
Nicole Catacutan <ngcatacutan@gmail.com>
2020-02-14 11:55:48 -0800

    Initial commit

---
+++ assignment.js
@@ -0,0 +1,18 @@
+//Create a variable with a fictional, numerical user input
+var averageMph = 35;
+
+//Create an empty variable
+var result;
+
+//Reassign empty variable using variable holding a numerical value
+result = averageMph + 18; //53
+
+//Change the empty variable three times using operators
+result = result / 2; //26.5
+result = result + 10; //36.5
+result = result * 0.5; //18.25
+
+
+//Use alert() to display what values each variable are holding
+alert(averageMph);
+alert(result);
